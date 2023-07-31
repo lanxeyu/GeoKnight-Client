1ce05762a1ab9d5fa2dd83146cc83a5c1cdb4675
emptybagelman <samuelmcgin@aol.com>
2023-07-31 16:54:32 +0100

    adding healthbar, damage tip, combat box & attack button

--- scripts/battle.js
+++
@@ -1,7 +0,0 @@
-const playerHealthbar = window.getComputedStyle(document.querySelector(".player"),":before");
-const playerAtk = window.getComputedStyle(document.querySelector(".player"),":after");
-
-
-
-console.log(playerHealthbar.getPropertyValue("background-color"))
-console.log(playerAtk.getPropertyValue("background-color"))
