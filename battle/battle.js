730bc4f8148648469977564a2d0d2c8cec7b0d53
emptybagelman <samuelmcgin@aol.com>
2023-08-02 11:32:49 +0100

    refactored JS into seperate files

--- battle/battle.js
+++ battle/battle.js
@@ -1,3 +1,14 @@
+import {createEnemy,
+    getRandomInt,resolveAttack,
+    getObject,updateEnemyID,
+    checkZeroHP} from "./battle_logic.js";
+import {renderHPStat,
+    renderPlayerAttack,
+    renderHP,
+    destroyEnemyElement,
+    runTypeAnimation,
+    runShakeAnimation,
+    runDeathAnimation} from "./battle_rendering.js";
 
 // Initialize difficulty scalers
 let battleCounter = 1;
@@ -19,12 +30,11 @@ const player = {
     }
 };
 
-var enemyAttackMidpoint = 5;
-var enemyMaxHPMidpoint = 10;
-
 // Create empty array to store enemies
 let enemyArray = [];
 
+var enemyAttackMidpoint = 5;
+var enemyMaxHPMidpoint = 10;
 
 // Class constructor for enemy objects
 class Enemy {
@@ -43,83 +53,92 @@ class Enemy {
     }
 }
 
+export {Enemy,enemyArray,playerElement};
+
 // Create enemy elements for front end
-function enemyElements(){
-    const contDiv = document.createElement("div");
-    contDiv.className = "char enemy";
+// function enemyElements(){
+//     const contDiv = document.createElement("div");
+//     contDiv.className = "char enemy";
 
-    const enemySpan = document.createElement("span");
-    enemySpan.textContent = "Enemy";
+//     const enemySpan = document.createElement("span");
+//     enemySpan.textContent = "Enemy";
 
-    const hp = document.createElement("div");
-    hp.className = "healthbar";
-    const hpSpan = document.createElement("span");
-    hpSpan.textContent = "0";
+//     const hp = document.createElement("div");
+//     hp.className = "healthbar";
+//     const hpSpan = document.createElement("span");
+//     hpSpan.textContent = "0";
 
-    hp.appendChild(hpSpan);
+//     hp.appendChild(hpSpan);
 
-    const dmg = document.createElement("div");
-    dmg.className = "dmgstat";
-    dmg.textContent = "0";
+//     const dmg = document.createElement("div");
+//     dmg.className = "dmgstat";
+//     dmg.textContent = "0";
 
-    contDiv.appendChild(enemySpan);
-    contDiv.appendChild(hp);
-    contDiv.appendChild(dmg);
+//     contDiv.appendChild(enemySpan);
+//     contDiv.appendChild(hp);
+//     contDiv.appendChild(dmg);
 
-    return contDiv;
-}
+//     return contDiv;
+// }
 
 // Create and add an enemy to the enemyArray
-function createEnemy(maxHP, attack) {
-    let len = enemyArray.length;
+// function createEnemy(maxHP, attack) {
+//     let len = enemyArray.length;
 
-    const newEnemy = new Enemy(maxHP, attack, len);
-    enemyArray.push(newEnemy);
-    renderEnemy(len);
-    // return newEnemy; // Optional return, might need later
-}
+//     const newEnemy = new Enemy(maxHP, attack, len);
+//     enemyArray.push(newEnemy);
+//     renderEnemy(len);
+//     // return newEnemy; // Optional return, might need later
+// }
 
 // Render enemies on screen
-function renderEnemy(len){
-    // add id to enemy
-    const enEl = enemyElements();
-    enEl.id = `en${len}`;
-    // render enemy dmg stat
-    enEl.querySelector(".dmgstat").textContent = enemyArray[len].attack;
-    // add to body
-    if(len>2){enEl.classList.add("hide")}
-    document.body.appendChild(enEl);
-    //render enemy hp value
-    renderHPStat(enemyArray[len]);
-}
+// function renderEnemy(len){
+//     // add id to enemy
+//     const enEl = enemyElements();
+//     enEl.id = `en${len}`;
+//     // render enemy dmg stat
+//     enEl.querySelector(".dmgstat").textContent = enemyArray[len].attack;
+//     // add to body
+//     if(len>2){enEl.classList.add("hide")}
+//     document.body.appendChild(enEl);
+//     //render enemy hp value
+//     renderHPStat(enemyArray[len]);
+// }
 
 // Increase number of enemies and enemy stats based on battleCounter 
+// function adjustDifficulty() {
+    
+//     enemyCount = 1 + Math.floor(battleCounter * 0.5);
+//     enemyMaxHPMidpoint = 4 + Math.floor(battleCounter * 1.7);
+//     enemyAtkMidpoint = 2 + Math.floor(battleCounter * 1.4);
+// }
+
+
+// function getRandomInt(midpoint,range) {
+//     const min = midpoint - range;
+//     const max = midpoint + range;
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+//shows atk stat on screen
+// function renderPlayerAttack(){
+//     const dmgEl = document.querySelector(".player").querySelector(".dmgstat");
+//     dmgEl.textContent = player.attack;
+// }
+
+//shows value of hp on screen
+// function renderHPStat(object){
+//     let id = getObject(object);
+//     //target obj
+//     const hpTextEl = document.getElementById(id).querySelector(".healthbar").querySelector("span");
+//     //target psuedo element that holds value
+//     hpTextEl.textContent = object.currHP;    
+// }
+
 function adjustDifficulty() {
     enemyCount = 1 + Math.floor(battleCounter * 0.5);
     enemyMaxHPMidpoint = 4 + Math.floor(battleCounter * 1.7);
-    enemyAtkMidpoint = 2 + Math.floor(battleCounter * 1.4);
-}
-
-
-function getRandomInt(midpoint,range) {
-    const min = midpoint - range;
-    const max = midpoint + range;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-//shows atk stat on screen
-function renderPlayerAttack(){
-    const dmgEl = document.querySelector(".player").querySelector(".dmgstat");
-    dmgEl.textContent = player.attack;
-}
-
-//shows value of hp on screen
-function renderHPStat(object){
-    let id = getObject(object);
-    //target obj
-    const hpTextEl = document.getElementById(id).querySelector(".healthbar").querySelector("span");
-    //target psuedo element that holds value
-    hpTextEl.textContent = object.currHP;    
+    enemyAttackMidpoint = 2 + Math.floor(battleCounter * 1.4);
 }
 
 function initBattlePhase() {
@@ -133,111 +152,111 @@ function initBattlePhase() {
     }
 }
 
-function resolveAttack(player, enemy) {
-    player.currHP -= enemy.attack
-    playerATK = player.attack + (getRandomInt(player.attack,1))
-    enemy.currHP -= playerATK
+// function resolveAttack(player, enemy) {
+//     player.currHP -= enemy.attack
+//     playerATK = player.attack + (getRandomInt(player.attack,1))
+//     enemy.currHP -= playerATK
 
-    runHitAnimation(playerElement)
-    runHitAnimation(document.getElementById("en0"))
-    runTypeAnimation(`GeoKnight dealt ${playerATK} damage to the enemy.`);
-    setTimeout(function() {runTypeAnimation(`Enemy dealt ${enemyArray[0].attack} damage to the GeoKnight.`)},1200);
-}
+//     runHitAnimation(playerElement)
+//     runHitAnimation(document.getElementById("en0"))
+//     runTypeAnimation(`GeoKnight dealt ${playerATK} damage to the enemy.`);
+//     setTimeout(function() {runTypeAnimation(`Enemy dealt ${enemyArray[0].attack} damage to the GeoKnight.`)},1200);
+// }
 
 
-function checkZeroHP(object) {
-    return object.currHP <= 0;
-}
+// function checkZeroHP(object) {
+//     return object.currHP <= 0;
+// }
 
-function getObject(object){
-    if(String(object.id).includes("en")){
-        return object.id;
-    }else if(object.name == "player"){
-        return player.name;
-    }
-}
+// function getObject(object){
+//     if(String(object.id).includes("en")){
+//         return object.id;
+//     }else if(object.name == "player"){
+//         return player.name;
+//     }
+// }
 
 //sets healthbar width based on current hp values
-function renderHP(object){
-    renderHPStat(object)
-    let obj = getObject(object);
+// function renderHP(object){
+//     renderHPStat(object)
+//     let obj = getObject(object);
 
-    const healthbar = document.getElementById(obj).querySelector(".healthbar");
-    const hbWidth = Number(window.getComputedStyle(healthbar).width.substring(0,window.getComputedStyle(healthbar).width.length-2));
-    // hp lost in relation to healthbar width
-    lostHP = ((object.maxHP-object.currHP)/object.maxHP)*hbWidth;
-    // styling health lost
-    healthbar.style = `box-shadow: inset ${-lostHP}px 0 0 0 black,0px -5px 0 0 rgb(192, 0, 0) inset`;
-}
+//     const healthbar = document.getElementById(obj).querySelector(".healthbar");
+//     const hbWidth = Number(window.getComputedStyle(healthbar).width.substring(0,window.getComputedStyle(healthbar).width.length-2));
+//     // hp lost in relation to healthbar width
+//     lostHP = ((object.maxHP-object.currHP)/object.maxHP)*hbWidth;
+//     // styling health lost
+//     healthbar.style = `box-shadow: inset ${-lostHP}px 0 0 0 black,0px -5px 0 0 rgb(192, 0, 0) inset`;
+// }
 
 // destroys HTML elements of enemy after death
-function destroyEnemyElement(){
-    const frontArray = enemyArray[0].id;
-    document.querySelector(`#${frontArray}`).remove();
-}
+// function destroyEnemyElement(){
+//     const frontArray = enemyArray[0].id;
+//     document.querySelector(`#${frontArray}`).remove();
+// }
 
 // updates HTML and JS id for enemy, so they move forwards
-function updateEnemyID(){
-    for(let i=0;i<enemyArray.length;i++){
-        const enemyEl = document.getElementById(`en${i+1}`);
-        // HTML element
-        enemyEl.id = `en${i}`;
-        if(i<=2){enemyEl.classList.remove("hide")}
-        //JS element
-        enemyArray[i].id = `en${i}`;
-    }
-}
+// function updateEnemyID(){
+//     for(let i=0;i<enemyArray.length;i++){
+//         const enemyEl = document.getElementById(`en${i+1}`);
+//         // HTML element
+//         enemyEl.id = `en${i}`;
+//         if(i<=2){enemyEl.classList.remove("hide")}
+//         //JS element
+//         enemyArray[i].id = `en${i}`;
+//     }
+// }
 
 //typewriter animation on textbox
-function runTypeAnimation(text){
-    //grey out ATTACK button
-    const atkBtn = document.getElementById("attack-btn")
-    atkBtn.style.cursor = "not-allowed";
-    atkBtn.style.filter = "brightness(0.6)";
+// function runTypeAnimation(text){
+//     //grey out ATTACK button
+//     const atkBtn = document.getElementById("attack-btn")
+//     atkBtn.style.cursor = "not-allowed";
+//     atkBtn.style.filter = "brightness(0.6)";
 
-    const textbox = document.querySelector(".fight-container").children[0];
-    const stepVariable = document.documentElement;
-    stepVariable.style.setProperty("--letter-steps",text.length);
+//     const textbox = document.querySelector(".fight-container").children[0];
+//     const stepVariable = document.documentElement;
+//     stepVariable.style.setProperty("--letter-steps",text.length);
 
-    textbox.style.display = "inline-block";
-    textbox.textContent = text;
-    textbox.classList.add("typeW");
-    textbox.addEventListener("animationend", () => {
-        textbox.classList.remove("typeW");
+//     textbox.style.display = "inline-block";
+//     textbox.textContent = text;
+//     textbox.classList.add("typeW");
+//     textbox.addEventListener("animationend", () => {
+//         textbox.classList.remove("typeW");
 
-        atkBtn.style.cursor = "pointer";
-        atkBtn.style.filter = "brightness(1)";
-    },5000);
-}
+//         atkBtn.style.cursor = "pointer";
+//         atkBtn.style.filter = "brightness(1)";
+//     },5000);
+// }
 
-// background animation shake
-function runShakeAnimation(){
-    const bg = document.getElementById("background");
-    bg.classList.add("shake");
-    bg.addEventListener("animationend",() => {
-        bg.classList.remove("shake");
-    })
-}
+// // background animation shake
+// function runShakeAnimation(){
+//     const bg = document.getElementById("background");
+//     bg.classList.add("shake");
+//     bg.addEventListener("animationend",() => {
+//         bg.classList.remove("shake");
+//     })
+// }
 
-function runHitAnimation(object){
-    const victimEl = document.getElementById(object.id);
-    victimEl.classList.add("hurt");
-    victimEl.addEventListener("animationend",()=>{
-        victimEl.classList.remove("hurt");
-    })
-}
+// function runHitAnimation(object){
+//     const victimEl = document.getElementById(object.id);
+//     victimEl.classList.add("hurt");
+//     victimEl.addEventListener("animationend",()=>{
+//         victimEl.classList.remove("hurt");
+//     })
+// }
 
-function runDeathAnimation(object){
-    const victimEl = document.getElementById(object.id);
-    victimEl.classList.add("dead");
-    for(let i=0;i<victimEl.children.length;i++){
-        victimEl.children[i].style.display = "none";
-    }
-    victimEl.addEventListener("animationend", ()=>{
-        victimEl.classList.remove("dead");
-        victimEl.style.display = "none";
-    })
-}
+// function runDeathAnimation(object){
+//     const victimEl = document.getElementById(object.id);
+//     victimEl.classList.add("dead");
+//     for(let i=0;i<victimEl.children.length;i++){
+//         victimEl.children[i].style.display = "none";
+//     }
+//     victimEl.addEventListener("animationend", ()=>{
+//         victimEl.classList.remove("dead");
+//         victimEl.style.display = "none";
+//     })
+// }
 
 // function runBattlePhase() {
     // Event listener for the attack button
