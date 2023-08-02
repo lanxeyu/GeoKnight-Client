f1bae47a3c1fb6c168ca66cc39579d4187331dd3
“rkumari2” <rubinakumari@mail.com>
2023-08-02 15:39:52 +0100

    separate questions in 3 files and connected them to the categories page

--- choice_page/index.js
+++ choice_page/index.js
@@ -1,35 +1,46 @@
 const categories = document.querySelector('#categories')
 
-function displayChoices () {
-    fetch('http://localhost:3000/levels')
-    .then (resp => resp.json())
-    .then (data => {
-        const options = data; 
-        console.log(options)
-        const easy = document.querySelector('#easy')
-        easy.textContent = options[0]
-        const medium = document.querySelector('#medium')
-        medium.textContent = options[1]
-        const hard = document.querySelector('#hard')
-        hard.textContent = options[2]
-        const li = document.createElement('li')
-        li.textContent = 'Lorem ipsum text placeholder'
-        easy.appendChild(li)
-        const li2 = document.createElement('li')
-        li2.textContent = 'Lorem ipsum text placeholder'
-        medium.appendChild(li2)
-        const li3 = document.createElement('li')
-        li3.textContent = 'Lorem ipsum text placeholder'
-        hard.appendChild(li3)
-    })
+function displayEasy () {
+    fetch('http://localhost:3000/levels/easy')
+        .then(resp => resp.json())
+        .then(data => {
+            const easy = document.querySelector('#easy');
+            easy.textContent = "Easy";
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
 }
 
-displayChoices()
+
+function displayMedium() {
+    fetch('http://localhost:3000/levels/medium')
+    .then(resp => resp.json())
+    .then(data => {
+        const medium = document.querySelector('#medium')
+        medium.textContent = "Medium"
+    })
+    .catch(error => {
+        console.error("Error fetching data:", error);
+    });
+}
 
 
+function displayHard() {
+    fetch('http://localhost:3000/levels/hard')
+    .then(resp => resp.json())
+    .then(data => {
+        const hard = document.querySelector('#hard')
+        hard.textContent = "Hard"
+    })
+    .catch(error => {
+        console.error("Error fetching data:", error);
+    });
+}
 
 
-
-
+displayMedium()
+displayEasy()
+displayHard()
 
 
---
+++ questions_page/easy.js
@@ -0,0 +1,21 @@
+function displayEasyQuestion () {
+    const displayedquestion = document.querySelector('h3')
+    const option1 = document.querySelector('#option1')
+    const option2 = document.querySelector('#option2')
+    const option3 = document.querySelector('#option3')
+    const option4 = document.querySelector('#option4')
+    fetch ('http://localhost:3000/levels/easy/random')
+    .then (resp => resp.json())
+    .then (data => {
+        displayedquestion.textContent = data.question
+        option1.textContent = data.choice1
+        option2.textContent = data.choice2
+        option3.textContent = data.choice3
+        option4.textContent = data.choice4
+    })
+    .catch (err => {
+        console.log(err)
+    })
+}
+
+displayEasyQuestion()
