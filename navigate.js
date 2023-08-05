1e9a19c36270c67605786d851d923bdf01bb2098
lanxeyu <lanxeyu@gmail.com>
2023-08-05 16:53:52 +0100

    Remove unused event listener

--- navigate.js
+++ navigate.js
@@ -5,22 +5,6 @@ document.getElementById("start").addEventListener("click",() => {
     location.href = "./choice_page/categories.html";
 })
 
-document.getElementById("rules").addEventListener("click",()=>{
-    window.alert(`Draw power from mending the shattered world, and crush adversaries hell-bent on thwarting your noble quest!
-    
-How to Play:
-
-The game cycles between 2 phases:
-
-Mend the World
-Choose a power-up, then answer the question correctly to increase your stats!
-    
-Enemy Invasion
-Press the attack button to slay your enemies!
-    
-Put the pieces of the world back together while fending off enemies for as long as you can to attain a high score. Good luck GeoKnight!`);
-})
-
 document.getElementById("scoreboard").addEventListener("click",()=>{
     location.href = "./scoreboard/index.html";
 })
